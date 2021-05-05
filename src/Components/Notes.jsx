feb73c3e8de76f69688f81918a2bc1208fe2682a
Vinay Gupta <vinaytest75@gmail.com>
2021-05-05 16:38:19 +0530

    css masonry package used

--- src/Components/Notes.jsx
+++ src/Components/Notes.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
 import NoteCard from "./NoteCard";
+import Masonry from "react-masonry-css";
 
 export default function Notes() {
   const [notes, setNotes] = useState([]);
@@ -13,7 +14,7 @@ export default function Notes() {
   }, []);
 
   const handleDelete = async (id) => {
-    await fetch("https://material-ui-tut.herokuapp.com/notes" + id, {
+    await fetch("https://material-ui-tut.herokuapp.com/notes/" + id, {
       method: "DELETE",
     });
 
@@ -21,15 +22,25 @@ export default function Notes() {
     setNotes(newNotes);
   };
 
+  const breakpoints = {
+    default: 3,
+    1100: 2,
+    700: 1,
+  };
+
   return (
     <Container>
-      <Grid container spacing={3}>
+      <Masonry
+        breakpointCols={breakpoints}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
         {notes.map((note) => (
-          <Grid item key={note.id} xs={12} md={6} lg={4}>
-            <NoteCard note={note} handleDelete={handleDelete}/>
-          </Grid>
+          <div item key={note.id}>
+            <NoteCard note={note} handleDelete={handleDelete} />
+          </div>
         ))}
-      </Grid>
+      </Masonry>
     </Container>
   );
 }
