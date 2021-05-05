8e3c05a235eef767c1ea7b5a50274f07de7abde2
Vinay Gupta <vinaytest75@gmail.com>
2021-05-05 16:18:09 +0530

    avatar and Appbar added

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,31 +1,50 @@
-import { Drawer, makeStyles, Typography } from "@material-ui/core";
+import { Avatar, Drawer, makeStyles, Typography } from "@material-ui/core";
 import React from "react";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import { SubjectOutlined, AddCircleOutlineOutlined } from "@material-ui/icons";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
 import { useHistory, useLocation } from "react-router";
+import { format } from "date-fns";
 
 const drawerWidth = 240;
 
-const useStyles = makeStyles({
-  root: {
-    display: "flex",
-  },
-  page: {
-    background: "#f9f9f9",
-    width: "100%",
-  },
-  drawer: {
-    width: drawerWidth,
-  },
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  active: {
-    background: "#f4f4f4",
-  },
+const useStyles = makeStyles((theme) => {
+  return {
+    root: {
+      display: "flex",
+    },
+    page: {
+      background: "#f9f9f9",
+      width: "100%",
+      padding: theme.spacing(3),
+    },
+    drawer: {
+      width: drawerWidth,
+    },
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    active: {
+      background: "#f4f4f4",
+    },
+    title: {
+      padding: theme.spacing(2),
+    },
+    appbar: {
+      width: `calc(100% - ${drawerWidth}px)`,
+    },
+    toolbar: theme.mixins.toolbar,
+    date: {
+      flexGrow: 1,
+    },
+    avatar: {
+      marginLeft: theme.spacing(2),
+    },
+  };
 });
 
 function Layout({ children }) {
@@ -49,6 +68,18 @@ function Layout({ children }) {
   return (
     <div className={classes.root}>
       {/* app bar */}
+      <AppBar className={classes.appbar} elevation={0}>
+        <Toolbar>
+          <Typography className={classes.date}>
+            Today: {Date().toString()}
+          </Typography>
+          <Typography>Vinay</Typography>
+          <Avatar
+            src="https://avatars.githubusercontent.com/u/62155169?v=4"
+            className={classes.avatar}
+          />
+        </Toolbar>
+      </AppBar>
 
       {/* side drawer */}
       <Drawer
@@ -58,7 +89,9 @@ function Layout({ children }) {
         classes={{ paper: classes.drawerPaper }}
       >
         <div>
-          <Typography variant="h5">Notes</Typography>
+          <Typography variant="h5" className={classes.title}>
+            Notes
+          </Typography>
         </div>
 
         <List>
@@ -76,7 +109,10 @@ function Layout({ children }) {
         </List>
       </Drawer>
 
-      <div className={classes.page}>{children}</div>
+      <div className={classes.page}>
+        <div className={classes.toolbar}></div>
+        {children}
+      </div>
     </div>
   );
 }
--- src/Components/NoteCard.jsx
+++ src/Components/NoteCard.jsx
@@ -2,14 +2,40 @@ import React from "react";
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardContent from "@material-ui/core/CardContent";
-import { IconButton, Typography } from "@material-ui/core";
+import { IconButton, makeStyles, Typography } from "@material-ui/core";
 import DeleteOutlinedIcon from "@material-ui/icons/DeleteOutlined";
+import Avatar from "@material-ui/core/Avatar";
+import { blue, pink, yellow } from "@material-ui/core/colors";
+
+const useStyles = makeStyles({
+  avatar: {
+    backgroundColor: (note) => {
+      if (note.category == "work") {
+        return yellow[700];
+      }
+      if (note.category == "money") {
+        return yellow[500];
+      }
+      if (note.category == "todos") {
+        return pink[500];
+      }
+      return blue[500];
+    },
+  },
+});
 
 function NoteCard({ note, handleDelete }) {
+  const classes = useStyles(note);
+
   return (
     <div>
       <Card elevation={1}>
         <CardHeader
+          avatar={
+            <Avatar className={classes.avatar}>
+              {note.category[0].toUpperCase()}
+            </Avatar>
+          }
           action={
             <IconButton onClick={() => handleDelete(note.id)}>
               <DeleteOutlinedIcon />
